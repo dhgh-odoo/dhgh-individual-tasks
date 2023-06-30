9bbf32e6dd9d9daec663b3eead899b76c1c38a65
Dheeraj Gharde <dhgh@odoo.com>
2023-06-30 14:27:30 -0400

    [IMP] Code Review Changes
    Incorporated the changes requested on the PR
    
    ID=3362270

--- task1_materials_castelar/__manifest__.py
+++ task1_materials_castelar/__manifest__.py
@@ -1,10 +1,11 @@
 {
-    'name': "Task1 Materials Castelar",
+    'name': "Materials_Castelar_Stock",
     'description': "Implementation for the task: 'Materiales Castelar: Warehouse shouldn't receive more than ordered quantity'",
     'sequence': 315,
     'version': '1.0',
-    'application': True,
+    'application': False,
     'depends': ['stock'],
     'data': [],
     'license': 'OEEL-1',
 }
+
--- task1_materials_castelar/models/stock_move.py
+++ task1_materials_castelar/models/stock_move.py
@@ -4,7 +4,7 @@ from odoo.exceptions import UserError
 class StockMove(models.Model):
     _inherit = "stock.move"
     
-    @api.onchange('product_uom_qty','quantity_done')
+    @api.constrains('product_uom_qty','quantity_done')
     def _check_quantity_done(self):
         '''
         Checks if quantity_done must be equal or less than the demand for receipts in the Invetory
@@ -12,6 +12,7 @@ class StockMove(models.Model):
         :param self - set of records associated to the context where this function is being called
         '''
         for record in self:
-            picking_type_record = self.env['stock.picking.type'].search([('id','=',record.picking_type_id.id)])
+            picking_type_record = self.env['stock.picking.type'].browse([(record.picking_type_id.id)])
             if picking_type_record.code=="incoming" and record.quantity_done > record.product_uom_qty:
-                raise UserError('Odoopsie! Quantity done must be less than or equal to demand')
+                raise UserError('Odoopsie! You can\'t receive more than the ordered quantity. Please, enter another quantity')
+            
