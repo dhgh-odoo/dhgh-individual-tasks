7229febea016cbd88f0d610875495de0a0b8efe5
Dheeraj Gharde <dhgh@odoo.com>
2023-06-30 14:32:47 -0400

    [IMP] Manifest File
    Uppdated name in manifets file
    
    ID=3362285

--- task2_link_source/__manifest__.py
+++ task2_link_source/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    'name': 'LinkSource_Sale',
+    'name': 'LinkSource_sale',
     'description': 'Implementation for the task: "LinkSource Pricing Update"',
     'summary': 'Removed the Auto Updation of Unit Price when you update the quantity',
     'author': 'Odoo Inc.',
